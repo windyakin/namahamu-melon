61708ee3c1fe75eab484d59c091cce2a98196243
windyakin <windyakin@gmail.com>
2018-03-26 07:59:47 +0900

    Add Logger(morgan)

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 const Melon = require('./modules/melon.js');
 const Namahamu = require('./modules/namahamu.js');
+
+app.use(morgan('combined'));
 
 app.get('/melon/:id', async (req, res) => {
   const bookId = req.params.id;
