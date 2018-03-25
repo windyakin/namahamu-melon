2235357a7a00480c5b26d1553d7e21659d84d293
windyakin <windyakin@gmail.com>
2018-03-26 00:19:22 +0900

    Namahamu Melon

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Hello, world!');
+const express = require('express');
+
+const app = express();
+
+const Melon = require('./modules/melon.js');
+const Namahamu = require('./modules/namahamu.js');
+
+app.get('/melon/:id', async (req, res) => {
+  const bookId = req.params.id;
+  const melon = await new Melon();
+  try {
+    const values = await melon.getBookAttributes(bookId);
+    const book = new Namahamu(values);
+    res.json(book);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await melon.close();
+  }
+});
+
+app.listen(3000);
