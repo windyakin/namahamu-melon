4e75877b9db7aa2cba5b72187a21011b0b38c897
windyakin <windyakin@gmail.com>
2020-08-03 22:05:29 +0900

    Support Tiger

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const helmet = require('helmet');
 
 const ScrapeDriver = require('./modules/scrapeDriver.js');
 const Melon = require('./modules/melon.js');
+const Tiger = require('./modules/tiger.js');
 
 const app = express();
 let scrapeDriver = null;
 let melon = null;
+let tiger = null;
 let server = null;
 
 app.use(helmet());
@@ -35,9 +37,27 @@ app.get('/melon/:id', async (req, res) => {
   }
 });
 
+app.get('/tiger/:id', async (req, res) => {
+  try {
+    const book = await tiger.getNamahamu(req.params.id);
+    res.json(book);
+  } catch (e) {
+    switch (e.message) {
+      case 'NotFound':
+        res.status(404).json({ statusCode: 404 });
+        break;
+      default:
+        console.log(e);
+        res.status(500).json({ statusCode: 500 });
+        break;
+    }
+  }
+});
+
 (async () => {
   scrapeDriver = await new ScrapeDriver();
   melon = new Melon(scrapeDriver);
+  tiger = new Tiger(scrapeDriver);
   server = app.listen(3000);
 })();
 
